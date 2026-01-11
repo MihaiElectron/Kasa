7501095ea0473432b336e1d4994089a51726b7f8
Mihai Radulescu <mihaielectron@gmail.com>
2026-01-11 14:01:49 +0100

    feat(header): création du header responsive (desktop et mobile) avec navigation active

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,43 @@
+import { NavLink } from 'react-router-dom'
 import logo from '../../assets/images/logo-kasa.png'
 
 function Header() {
-    return (
-      <header className="header">
-        <div className="header__container">
-          <img
-            src={logo}
-            alt="Kasa"
-            className="header__logo"
-          />
-  
-          <nav className="header__nav">
-            <ul className="header__list">
-              <li className="header__item">
-                <a href="/" className="header__link">
-                  Accueil
-                </a>
-              </li>
-              <li className="header__item">
-                <a href="/about" className="header__link">
-                  À propos
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    )
-  }
-  
-  export default Header
-  
+  return (
+    <header className="header">
+      <div className="header__container">
+        <img
+          src={logo}
+          alt="Kasa"
+          className="header__logo"
+        />
+
+        <nav className="header__nav">
+          <ul className="header__list">
+            <li className="header__item">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? 'header__link header__link--active' : 'header__link'
+                }
+              >
+                Accueil
+              </NavLink>
+            </li>
+            <li className="header__item">
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  isActive ? 'header__link header__link--active' : 'header__link'
+                }
+              >
+                A propos
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header/Header'
 
+
 function MainLayout() {
   return (
     <>
-      <Header />
-      <Outlet />
+      <div className="layout-container">
+      <Header />             {/* toujours visible */}
+        <Outlet />           {/* affichera Home, About, Housing, Error */}
+      </div>
     </>
   )
 }
