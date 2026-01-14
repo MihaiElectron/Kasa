3b5a8f04e00956beecf65429ff55f175055b2fc9
Mihai Radulescu <mihaielectron@gmail.com>
2026-01-14 12:56:21 +0100

    feat(error): stylisation de la page 404 et ajout du lien de retour vers l’accueil

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,20 @@
 // src/pages/Error.jsx
+import { Link } from 'react-router-dom';
+
 function Error() {
-    return (
-      <div>
-        <h1>Page 404</h1>
-      </div>
-    );
-  }
-  
-  export default Error;
-  
+  return (
+    <main className="error">
+      <h1 className="error__code">404</h1>
+
+      <p className="error__message">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+
+      <Link to="/" className="error__link">
+        Retourner sur la page d’accueil
+      </Link>
+    </main>
+  );
+}
+
+export default Error;
